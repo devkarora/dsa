f9a5232c3f9c8fd89c228cd1dc2ab5a5e74ee4e0
Dev Kumar Arora <devkumararora7@gmail.com>
2020-06-03 13:37:36 +0530

    a

--- sorting/optimised bubblesort.cpp
+++ sorting/optimised bubblesort.cpp
@@ -1,10 +1,54 @@
 //bubble sort Optimized Implementation if the inner loop doesnt swap , them the array is sorted already
 
-https://www.geeksforgeeks.org/bubble-sort/
+//https://www.geeksforgeeks.org/bubble-sort/
 
 #include <bits/stdc++.h>
 using namespace std;
 
+//PURE RECURSIVE NON OPTIMISED
+void bubblesort_pure_recursive(int arr[],int n,int i){
+    
+    if(n==1){
+        return;
+    }
+    if(i==n-1){
+        bubblesort_pure_recursive(arr,n-1,0);
+        return;
+    }
+    
+    
+    if(arr[i] > arr[i+1]){
+        int temp=arr[i];
+        arr[i]=arr[i+1];
+        arr[i+1]=temp;
+    }
+    bubblesort_pure_recursive(arr,n,i+1);
+    return;
+}
+
+//SEMI RECURSIVE OPTIMISED
+void bubblesort_recursive_optimised(int arr[],int n,int i){
+    
+    if(n==1){
+        return;
+    }
+    bool is_swap =0;
+    for(int i=0;i<n-1;i++){
+        if(arr[i] > arr[i+1]){
+            int temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
+            is_swap=1;
+        }
+    }
+    if(!is_swap){
+        return;
+    }
+    bubblesort_recursive_optimised(arr,n-1,0);
+    return;
+}
+
+//ITERATIVE OPTIMISED
 void bubblesort(int arr[], int n){
     bool is_swap_done;
     for(int i=0;i<n-1;i++){
@@ -33,9 +77,13 @@ void print(int arr[], int n){
 }
 int main()
 {
-    int arr[]= { 9 , 8, 4, 3,5 ,6 ,7 };
+    int arr[]= { 9, 8, 4, 3 ,5 ,6 ,7 };
     int n = sizeof(arr)/sizeof(arr[0]);
     bubblesort(arr,n);
     print(arr,n);
     return 0;
 }
+
+
+
+
