b1fe86ede69abd4dcb870ceeb13a7d2965dc054b
Dev Kumar Arora <devkumararora7@gmail.com>
2020-06-03 19:44:43 +0530

    a

--- sorting/insertion & binary insertion sort.cpp
+++ sorting/insertion & binary insertion sort.cpp
@@ -1,1 +1,69 @@
-wfdssdf
+//INSERTION SORT
+https://www.geeksforgeeks.org/insertion-sort/
+
+void insertionSort(int arr[], int n)  
+{  
+    int i, key, j;  
+    for (i = 1; i < n; i++) 
+    {  
+        key = arr[i];  
+        j = i - 1;  
+  
+        /* Move elements of arr[0..i-1], that are  
+        greater than key, to one position ahead  
+        of their current position */
+        while (j >= 0 && arr[j] > key) 
+        {  
+            arr[j + 1] = arr[j];  
+            j = j - 1;  
+        }  
+        arr[j + 1] = key;  
+    }  
+}  
+
+//BINARY INSERTION SORT
+https://www.geeksforgeeks.org/binary-insertion-sort/
+in the above algorithm of insertion sort, instead of directly comparing and shifting every element, 
+    find out the location where the element is to be inserted and then shift one by one just like in the above algorithm
+    The algorithm as a whole still has a running worst case running time of O(n^2)
+    because of the series of swaps required for each insertion
+    
+   // A binary search based function to find the position 
+// where item should be inserted in a[low..high] 
+int binarySearch(int a[], int item, int low, int high) 
+{ 
+    if (high <= low) 
+        return (item > a[low])?  (low + 1): low; 
+  
+    int mid = (low + high)/2; 
+  
+    if(item == a[mid]) 
+        return mid+1; 
+  
+    if(item > a[mid]) 
+        return binarySearch(a, item, mid+1, high); 
+    return binarySearch(a, item, low, mid-1); 
+} 
+  
+// Function to sort an array a[] of size 'n' 
+void insertionSort(int a[], int n) 
+{ 
+    int i, loc, j, k, selected; 
+  
+    for (i = 1; i < n; ++i) 
+    { 
+        j = i - 1; 
+        selected = a[i]; 
+  
+        // find location where selected sould be inseretd 
+        loc = binarySearch(a, selected, 0, j); 
+  
+        // Move all elements after location to create space 
+        while (j >= loc) 
+        { 
+            a[j+1] = a[j]; 
+            j--; 
+        } 
+        a[j+1] = selected; 
+    } 
+} 
