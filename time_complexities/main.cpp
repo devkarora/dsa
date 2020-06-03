a9d6c637e8c1d2a9bca03d9329d4703804337588
Dev Kumar Arora <devkumararora7@gmail.com>
2020-06-03 17:30:34 +0530

    a

--- time_complexities/main.cpp
+++ time_complexities/main.cpp
@@ -1,4 +1,4 @@
-BUBBLE SORT :
+BUBBLE SORT : ( SORTED ARRAY FROM THE END , MAX GEOS AND SWAPS THE LAST)
   worst & average case : O(n^2) reverse sorted
   best case : O(N) already sorted
   space : O(1)
@@ -6,8 +6,13 @@ BUBBLE SORT :
   stable : yes
   https://www.geeksforgeeks.org/bubble-sort/
     
-    
-    
+SELECTION SORT : ( ALMOST LIKE BUBBLE SORT, MIN OF REMAINING ARRAY COMES IN THE FRONT AFTER EVERY ITERATION)
+  worst||average||best case : O(n^2) 
+  space : O(1)
+  no. of swaps in worst case : O(n)
+  in place : yes
+  stabel : no
+  
     
     
     
